d1f298f324204002d01e3b855154edfd8f9f7e88
S1R0K <vlads1r0k.06@gmail.com>
2026-09-05 14:32:10 +0300

    test render

--- app.py
+++ app.py
@@ -348,11 +348,8 @@ def create_app():
 
     return app
 
-app = create_app()
-
-with app.app_context():
-    db.create_all()
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app = create_app()
+    with app.app_context():
+        db.create_all()  # Create database tables if they don't exist
+    app.run(debug=False)
