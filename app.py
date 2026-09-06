aa1ba8da3e147aec677de4420eab14bd9f76a737
S1R0K <vlads1r0k.06@gmail.com>
2026-09-06 21:08:36 +0300

    nik

--- app.py
+++ app.py
@@ -102,17 +102,7 @@ def create_app():
 
     csrf.init_app(app)
     limiter.init_app(app)
-    Talisman(
-        app,
-        force_https=True,
-        strict_transport_security=True,
-        strict_transport_security_max_age=31536000,
-        content_security_policy={
-            'default-src': "'self'",
-            'script-src': ["'self'", "'unsafe-inline'"],
-            'style-src': ["'self'", "'unsafe-inline'"],
-        },
-    )
+    Talisman(app)
     mail = Mail(app)
     db.init_app(app)
 
